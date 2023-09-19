24cc8af5b951ce8eb282f18dcd694868def46cd4
ThineshKumar-S <thineshkumar97890@gmail.com>
2023-09-19 16:51:44 +0530

    final youtube engine modifications

--- youtube_engine.py
+++ youtube_engine.py
@@ -16,6 +16,7 @@ youtube_object = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
 #obtain channel_id from the user via user's input
 channel_id="UCjWY5hREA6FFYrthD0rZNIw"
 
+
 #get the channel info by this function.
 def get_channel_info(youtube_object,channel_id):
     
@@ -37,8 +38,7 @@ def get_channel_info(youtube_object,channel_id):
         Channel_data=[]
         request = youtube_object.channels().list(
             part="snippet,contentDetails,statistics",
-            id=channel_id
-            )
+            id=channel_id)
     
         response = request.execute()
     
@@ -52,13 +52,13 @@ def get_channel_info(youtube_object,channel_id):
                 'subcribers':item['statistics']['subscriberCount'],
                 'videos':item['statistics']['videoCount']
                 }
-        Channel_data.append(data)
+            Channel_data.append(data)
     
     except HttpError as e:
-    # Print the error details
-    print(f"HttpError: {e}")
-    print(f"Error content: {e.content}")
-    print(f"Error details: {e.resp}")
+        # Print the error details
+        print(f"HttpError: {e}")
+        print(f"Error content: {e.content}")
+        print(f"Error details: {e.resp}")
     
     return Channel_data
 
@@ -123,7 +123,7 @@ def get_video_ids(youtube_object, playlist_Id):
 
 
 #calling the function extract the list of video ids 
-video_ids=get_video_ids(youtube_object=youtube_object,playlist_Id=playlist_id)
+#video_ids=get_video_ids(youtube_object=youtube_object,playlist_Id=playlist_id)
 
 
 def get_video_info(youtube_object, video_ids):
@@ -151,9 +151,9 @@ def get_video_info(youtube_object, video_ids):
              )
         response = request.execute()
         for item in response['items']:
-             extracted_data = {'snippet': ['channelTitle', 'title', 'description', 'tags', 'publishedAt'],
-                             'statistics': ['viewCount', 'likeCount', 'favouriteCount', 'commentCount'],
-                             'contentDetails': ['duration', 'definition', 'caption']
+             extracted_data = {'snippet': ['channelTitle', 'description', 'publishedAt'],
+                             'statistics': ['viewCount', 'likeCount', 'commentCount'],
+                             'contentDetails': ['duration']
                               }
              video_info = {}
              video_info['video_id'] = item['id']
@@ -163,9 +163,120 @@ def get_video_info(youtube_object, video_ids):
                         video_info[value] = item[key][value]
                      except:
                         video_info[value] = None
-                 all_video_info.append(video_info) 
+             all_video_info.append(video_info) 
     return all_video_info
 
+#video_info=get_video_info(youtube_object=youtube_object,video_ids=video_ids)
 
 
 
+def get_comments(youtube_object, video_ids):
+    """
+    function description:
+    This function defined to fetch 
+    the video comments and replies from the youtube data api v3.
+
+    parameters: 
+    This function takes two parameters.
+    1.youtube connection object via googleapiclient.discovery
+    2.video id is used to to identify and 
+    extract the video info. 
+
+
+    returns: function retruns list of dictionary which contains 
+    the comments and replies with their video id .
+    
+    """
+
+    all_comments = []
+    
+    for video_id in video_ids:
+        try:
+            request = youtube_object.commentThreads().list(part="snippet,replies", videoId=video_id)
+            response = request.execute()
+            comments_and_replies = []
+            
+            for item in response['items']:
+                extract_data_comments = {
+                    'video_id': video_id, 
+                    'comment': item['snippet']['topLevelComment']['snippet']['textOriginal'],
+                    'comment_likes': item['snippet']['topLevelComment']['snippet']['likeCount']}
+                
+                no_of_replies = item['snippet']['totalReplyCount']
+                extracted_replies = []
+                
+                if no_of_replies:
+                    for i in range(no_of_replies):
+                        # Check if the index is within the valid range
+                        if i < len(item['replies']['comments']):
+                            reply = item['replies']['comments'][i]['snippet']
+                            extracted_reply = {
+                                'replies': reply['textOriginal'],
+                                'reply_likes': reply['likeCount']
+                            }
+                            extracted_replies.append(extracted_reply)
+                        else:
+                            extracted_reply = {
+                                'replies': 'No_reply for comment',
+                                'reply_likes': 0
+                            }
+                            extracted_replies.append(extracted_reply)
+                
+                Comments_replies = {'video_id': video_id,
+                    'Comments': extract_data_comments['comment'],
+                    'comment_likes': extract_data_comments['comment_likes'],
+                    'reply_count': no_of_replies,
+                    'replies': extracted_replies
+                    }
+                comments_and_replies.append(Comments_replies)
+            
+            all_comments.append(comments_and_replies)
+        
+        except HttpError as e:
+            # Handle the case where comments are disabled for the video
+            error_message = f"Comments are disabled for video ID {video_id}"
+            all_comments.append({'video_id': video_id,'Disabled_comments': error_message})
+    
+    return all_comments
+
+
+#comments=get_comments(youtube_object=youtube_object,video_ids=video_ids)
+
+def data(channel_id, Api_key):
+
+    """
+    function description:
+    This function defined to call the predefined functions to extract 
+    the channel's information and comment's & replies information.
+
+    parameters: 
+    This function takes two parameters 
+    1.channel_id
+    2.Api key for accessing the google services.
+    
+    returns: function retruns list of dictionary which contains 
+    the channel information, video information and comments& replies.
+    
+    """
+    API_KEY = Api_key
+    YOUTUBE_API_SERVICE_NAME = "youtube"
+    YOUTUBE_API_VERSION = "v3"
+    #creating Youtube connection Object
+    youtube_object = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                       developerKey = API_KEY)
+    # obtain channel_id from the user via user's input
+    
+    channel_id=channel_id
+
+    channel_data=get_channel_info(youtube_object=youtube_object,channel_id=channel_id)
+    playlist_id=channel_data[0]["playlsit_id"]
+    video_ids=get_video_ids(youtube_object=youtube_object,playlist_Id=playlist_id)
+    video_info=get_video_info(youtube_object=youtube_object,video_ids=video_ids)
+    comments_and_replies=get_comments(youtube_object=youtube_object,video_ids=video_ids)
+    
+    data=[channel_data,video_info,comments_and_replies]
+    
+    return data
+
+# data driver
+# data()
